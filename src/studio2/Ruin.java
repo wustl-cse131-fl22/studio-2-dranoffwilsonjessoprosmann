2e32d21106fc983565c7b4f16a6805414e57b28f
haleyrosmann <haleyrosmann@localhost>
2022-09-15 11:52:39 -0500

    Gambling

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,23 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("What is your starting amount?");
+		int startingAmount = in.nextInt();
+		
+		System.out.println("What is your win chance?");
+		double winChance = in.nextInt();
+		
+		System.out.println("What is your win limit?");
+		int winLimit = in.nextInt();
+		
+		
+	}
+
+}

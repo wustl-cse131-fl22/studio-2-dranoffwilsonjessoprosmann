2f90688a4885f33774648cb473eab0ddda307bed
mimi <mimi@mimis-mbp.wusm-wifi.wucon.wustl.edu>
2022-09-15 12:38:07 -0500

    Studio 2!

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -7,17 +7,43 @@ public class Ruin {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		
+
 		System.out.println("What is your starting amount?");
 		int startingAmount = in.nextInt();
-		
+
 		System.out.println("What is your win chance?");
-		double winChance = in.nextInt();
-		
+		double winChance = in.nextDouble();
+
 		System.out.println("What is your win limit?");
 		int winLimit = in.nextInt();
-		
-		
+
+		System.out.println("How many days will you simulate?");
+		int totalSimulations = in.nextInt();
+
+		for (int days = 0; days <= totalSimulations; days++) {
+			int money = startingAmount;
+			int numberOfPlays = 0;
+			System.out.println("Day " + days);
+			//will play until you hit max number of days 
+			while (money>0 && money < winLimit) {
+				numberOfPlays++;
+				if (Math.random() <= winChance) {
+					money += 1;
+				}
+				else {
+					money -= 1;
+				}
+			}
+			System.out.println("The number of plays is " + numberOfPlays);
+			if (money ==0) {
+				System.out.println("You lose!!");
+			}
+			else {
+				System.out.println("You win!");
+			}
+		}
 	}
 
 }
+
+
